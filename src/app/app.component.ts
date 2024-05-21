d3ba92f02e53142cb58bf3365a0bed5f03e56443
Retuart <pzx104888@student.wsb.poznan.pl>
2024-05-21 23:03:26 +0000

    feat: add basic footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  imports: [FooterComponent],
 })
 export class AppComponent { 
   title = "Gitarex Shop";
