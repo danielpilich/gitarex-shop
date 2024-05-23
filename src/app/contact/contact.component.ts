445aee451b1541e3b0dc9f5bc41a0caa5f4121f8
Franciszekpawlowski <franciszekpawlowskiit@gmail.com>
2024-05-23 19:51:44 +0000

    add discount-bar component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { DiscountBarComponent } from '../discount-bar/discount-bar.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,DiscountBarComponent],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
