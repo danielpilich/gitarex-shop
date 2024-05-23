db05e34f54f62011a84042e9a9f581e9799dc376
Retuart <pzx104888@student.wsb.poznan.pl>
2024-05-23 11:49:32 +0000

    chore(footer): component path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from '../footer/footer.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
