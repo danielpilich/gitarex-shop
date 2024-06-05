7274ace5eda1a21217f45d40156747b973c57955
Daniel Pilich <dpilich16@gmail.com>
2024-06-05 02:18:46 +0200

    Fix gaps, nomenclature and positions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
-import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
@@ -20,11 +19,4 @@ import { DiscountBarComponent } from './discount-bar/discount-bar.component';
 })
 export class AppComponent {
   title = "Gitarex Shop";
-
-  constructor(private router: Router) {}
-
-  navigateToProduct() {
-    console.log('Navigating to product page');
-    this.router.navigate(['/product']);
-  }
 }
--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 interface Category {
   icon: string;
@@ -14,13 +15,21 @@ interface Category {
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent {
+
+  constructor(private router: Router) {}
+
+  navigateToProduct() {
+    console.log('Navigating to product page');
+    this.router.navigate(['/product']);
+  }
+
   categories: Category[] = [
     { icon: 'assets/icons/guitar.png', name: 'Gitary' },
     { icon: 'assets/icons/microphone.png', name: 'Mikrofony' },
     { icon: 'assets/icons/cable.png', name: 'Kable' },
     { icon: 'assets/icons/bass.png', name: 'Basy' },
-    { icon: 'assets/icons/software.png', name: 'Oprogramowanie' },
-    { icon: 'assets/icons/effects.png', name: 'Efekty' },
+    { icon: 'assets/icons/software.png', name: 'Oprogramowania' },
+    { icon: 'assets/icons/keyboard.png', name: 'Klawisze' },
     { icon: 'assets/icons/drums.png', name: 'Perkusje' },
     { icon: 'assets/icons/amplifier.png', name: 'Wzmacniacze' }, 
     { icon: 'assets/icons/accessories.png', name: 'Akcesoria' }
